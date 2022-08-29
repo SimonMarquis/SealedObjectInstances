0ae8fbbe2c8059e316801da77a8fe918cf372ad2
Simon Marquis <contact@simon-marquis.fr>
2022-08-29 13:58:50 +0200

    🐘 Cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.jvm)
+    alias(libs.plugins.ksp)
 }
 
 repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,3 @@
-dependencyResolutionManagement {
-
-}
 pluginManagement {
     repositories {
         gradlePluginPortal()
