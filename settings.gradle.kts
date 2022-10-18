a0e5a054022d0e867538750baa45767bf16ace64
Simon Marquis <contact@simon-marquis.fr>
2022-10-18 20:24:35 +0200

    🐘 Enable Gradle scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,25 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version "3.11.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.2"
+}
+
 rootProject.name = "sealed-object-instances"
 include("processor")
 include("app")
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (System.getenv("CI").toBoolean()) {
+            publishAlways()
+            isUploadInBackground = false
+        }
+    }
+}
