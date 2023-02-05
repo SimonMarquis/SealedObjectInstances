61dae6cd0876fb9b2d93210d073d6a3474a88304
Simon Marquis <contact@simon-marquis.fr>
2023-02-05 14:28:54 +0100

    Update issue reference

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-@Suppress("DSL_SCOPE_VIOLATION") // https://youtrack.jetbrains.com/issue/KTIJ-19369
+@Suppress("DSL_SCOPE_VIOLATION") // https://github.com/gradle/gradle/issues/22797
 plugins {
     alias(libs.plugins.jvm)
     alias(libs.plugins.ksp)
