5ef4a3c2c0d1f5940cba0046306e532b668a48fe
Simon Marquis <contact@simon-marquis.fr>
2023-03-26 13:27:32 +0200

    Configure spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,38 @@ plugins {
     alias(libs.plugins.jvm) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.binaryCompatibilityValidator) apply false
+    alias(libs.plugins.spotless)
 }
 
 buildscript {
     dependencies {
         classpath(kotlin("gradle-plugin"))
+    }
+}
+
+allprojects {
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        val licenseHeader = rootProject.file("spotless/spotless.kt")
+        format("misc") {
+            target("**/*.md", "**/.gitignore")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+        kotlin {
+            target("src/**/*.kt")
+            ktlint(libs.versions.ktlint.get())
+            trimTrailingWhitespace()
+            endWithNewline()
+            licenseHeaderFile(licenseHeader)
+            targetExclude("spotless/*.kt")
+        }
+        kotlinGradle {
+            ktlint(libs.versions.ktlint.get())
+            trimTrailingWhitespace()
+            endWithNewline()
+            licenseHeaderFile(licenseHeader, "(import|plugins|buildscript|dependencies|pluginManagement|rootProject|@Suppress)")
+        }
     }
 }
 
